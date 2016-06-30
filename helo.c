c4759c7c92e3bcdd63abb0a5822ef26a5cbeb84c
Paarkavi <paarkavi12v@gmail.com>
2016-06-30 15:00:46 +0530

    Create helo.c

---
+++ helo.c
@@ -0,0 +1,9 @@
+
+TABLE OF CONTENTS
+-----------------
+1.  How to Get Help
+2.  Installation
+3.  Features
+4.  Important Information
+5.  Testing Your Expanded Memory
+6.  Corrections to the On-line Help
